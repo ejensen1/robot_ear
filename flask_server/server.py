ae546b304b39a75658dfad36d449c4fac6f01f6d
Rishita Wairagade <rwairagade@gmail.com>
2023-10-31 10:05:45 -0700

    flask server

---
+++ flask_server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/members")
+def members():
+    return {"members": ["rishita","tanya","melany"]}
+
+
+if __name__ == "__main__": 
+    app.run(debug=True)
