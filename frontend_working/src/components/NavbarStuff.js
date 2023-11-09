8c5504430ae2e4bb7d12206740e5b142d98a5c3b
Melany 🍬 <mdelcidc@ucsc.edu>
2023-11-08 17:27:42 -0800

    cleaned up the file upload code and added comments

--- frontend_working/src/components/NavbarStuff.js
+++ frontend_working/src/components/NavbarStuff.js
@@ -6,73 +6,28 @@ import axios from 'axios';
 // fixing input file tag from: https://jsfiddle.net/4cwpLvae/
 
 class NavbarStuff extends React.Component {
+  // file state
   state = {
- 
     // Initially, no file is selected
     selectedFile: null
 };
 
 // On file select (from the pop up)
 onFileChange = event => {
-    console.log("here")
     // Update the state
     this.setState({ selectedFile: event.target.files[0] });
+    // create a formData to format the file for sending to the server
     const fileData = new FormData();
+    // populate the formData object
     fileData.append("file", event.target.files[0]);
+    // post it using axios and console.log the response
+    // you should see the file you uploaded in flask_server/file_upload now
     axios.post("http://localhost:8080/upload", fileData)
     .then((response) => {
       console.log(response)
     })
   };
-    //fileData.append("filename", event.target.files[0]);
-    // fetch("http://localhost:8080/members",{
-    //   mode: 'no-cors'
-    // }).then(
-    //   res => res.json()
-    // ).then(
-    //   data => {
-    //     setData(data)
-    //     console.log(data)
-    //   }
-    // )
 
-
-//  getData = () => {
-//    if (this.state.selectedFile) {
-//     const fileData = new FormData();
-//     fileData.append("file", this.state.selectedFile);
-//     fileData.append("filename", this.state.selectedFile.name);
-//     console.log(fileData.get("file"));
-//     console.log(fileData.get("filename"));
-  
-//     // axios.post("api/upload", fileData);
-//     fetch('/upload', {
-//       method: 'POST',
-//       body: fileData,
-//     })
-//       .then(response => {
-//         if (response.ok) {
-//           return response.text();
-//         } else {
-//           throw new Error('File upload failed');
-//         }
-//       })
-//       .then(data => {
-//         console.log(data); // Handle the response from the Flask server
-//       })
-//       .catch(error => {
-//         console.error(error);
-//       });
-//     // fetch('/upload', {
-//     //   method: "POST",
-//     //   body: fileData,
-//     // }).then((response) => {
-//     //   response.text().then(data => alert(data));
-//     // });
-//     //  var fname = this.state.selectedFile.name;
-//     //  alert("file selected is: " + fname);
-//    }
-// }
 render () {
   return (
     <nav className="navbar">
@@ -82,7 +37,7 @@ render () {
       <img className="status-image-icon" alt="" src="/status-image@2x.png" />
       <div className="working">working...</div>
       <div className="upload-your-wav">Upload your .wav file</div>
-      <label for="file-upload" className="upload-file-image" id="upload_button"/>
+      <label for="file-upload" className="upload-file-image" id="upload_button"/>     
       <form method="POST" enctype="multipart/form-data">
       <input type="file" id="file-upload" accept=".wav" onChange={this.onFileChange}/>
       </form>
